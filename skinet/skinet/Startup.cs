22b40edeaa9d347aebc1482c78567649b9118c61
lonzoVS <vadshapran@gmail.com>
2020-11-14 18:56:51 +0200

    search, cors

--- skinet/skinet/Startup.cs
+++ skinet/skinet/Startup.cs
@@ -29,6 +29,13 @@ namespace skinet
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +49,10 @@ namespace skinet
             app.UseRouting();
             app.UseStaticFiles();
 
+            app.UseCors("CorsPolicy");
+
             app.UseAuthorization();
+
 
             app.UseSwaggerDocumentation();
 
